ddeec1d369d5950476a8676152b70bbe8d727084
jpwebdev <jjpwebdev@gmail.com>
2017-12-15 00:15:37 -0500

    deleted some comments and commented code. Only thing missing is the CSS

--- assets/js/app.js
+++ assets/js/app.js
@@ -70,7 +70,6 @@
                     $target.attr('data-status', 'static');
                     $target.attr('src', $img_src);
                 }
-                
             },
             create_btn(){
                 const new_topic = this.$input.val().toUpperCase();  // Get the user input and change it to upper case letters
@@ -94,36 +93,3 @@
     });
     
 })(jQuery); 
-
-    // *Start with a short Array (for topics)
-    // *(function populate()) populate the array items to the topic-btns
-    // *if the topic-btn is clicked
-        // *(function)
-        // *make an AJAX call
-        // *Get the GIF for the corresponding topic-btn
-        // *display 10 static imgs in the page
-        // *if img is clicked and !animate 
-            // *change static image for animated gif
-        // *if img is clicked and animate
-            // *change animated gif to static img
-    // if btn from the form is clicked
-        // get input value
-        // push it to the topic-array
-        // (function populate()) populate items to the btns
-
-    // Example queryURL for Giphy API
-    // var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=lFF9Yxkitf2ugL6Waes2cFHNZ8UE1h5i";
-    // var queryStr = "";
-    // $.ajax({
-    //   url: queryURL,
-    //   method: 'GET'
-    // }).done(function(response) {
-    //   let imgStill = response.data[0].images["480w_still"].url;
-
-    //   //let gifSrc = response.data[0].embed_url; 
-    //   let img = $('<img>').attr('src', imgStill);
-    //   console.log(imgStill);
-    //   $('#container').html(img);
-
-    //   console.log(response);
-    // });
